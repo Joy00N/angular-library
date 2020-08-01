6e2efe3b69b88c7ac9002ea1e45c527627baf4ca
Yoonhee <yoonhee.jo.cs@gmail.com>
2020-08-01 18:17:27 -0400

    generate library via ng packagr

---
+++ ng-packager-demo/super-lib/dist/esm2015/src/super-lib.component.js
@@ -0,0 +1,12 @@
+import * as tslib_1 from "tslib";
+import { Component } from "@angular/core";
+let SuperLibComponent = class SuperLibComponent {
+};
+SuperLibComponent = tslib_1.__decorate([
+    Component({
+        selector: 'super-lib-component',
+        template: '<p>Hello from the library</p>'
+    })
+], SuperLibComponent);
+export { SuperLibComponent };
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3VwZXItbGliLmNvbXBvbmVudC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uL3NyYy9zdXBlci1saWIuY29tcG9uZW50LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQSxPQUFPLEVBQUUsU0FBUyxFQUFFLE1BQU0sZUFBZSxDQUFDO0FBTTFDLElBQWEsaUJBQWlCLEdBQTlCLE1BQWEsaUJBQWlCO0NBQUcsQ0FBQTtBQUFwQixpQkFBaUI7SUFKN0IsU0FBUyxDQUFDO1FBQ1AsUUFBUSxFQUFFLHFCQUFxQjtRQUMvQixRQUFRLEVBQUUsK0JBQStCO0tBQzVDLENBQUM7R0FDVyxpQkFBaUIsQ0FBRztTQUFwQixpQkFBaUIiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBDb21wb25lbnQgfSBmcm9tIFwiQGFuZ3VsYXIvY29yZVwiO1xuXG5AQ29tcG9uZW50KHtcbiAgICBzZWxlY3RvcjogJ3N1cGVyLWxpYi1jb21wb25lbnQnLFxuICAgIHRlbXBsYXRlOiAnPHA+SGVsbG8gZnJvbSB0aGUgbGlicmFyeTwvcD4nXG59KVxuZXhwb3J0IGNsYXNzIFN1cGVyTGliQ29tcG9uZW50IHt9XG4iXX0=
---
+++ ng-packager-demo/super-lib/dist/esm2015/src/super-lib.module.js
@@ -0,0 +1,13 @@
+import * as tslib_1 from "tslib";
+import { NgModule } from "@angular/core";
+import { SuperLibComponent } from './super-lib.component';
+let SuperLibModule = class SuperLibModule {
+};
+SuperLibModule = tslib_1.__decorate([
+    NgModule({
+        declarations: [SuperLibComponent],
+        exports: [SuperLibComponent]
+    })
+], SuperLibModule);
+export { SuperLibModule };
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3VwZXItbGliLm1vZHVsZS5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uL3NyYy9zdXBlci1saWIubW9kdWxlLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQSxPQUFPLEVBQUUsUUFBUSxFQUFFLE1BQU0sZUFBZSxDQUFDO0FBQ3pDLE9BQU8sRUFBRSxpQkFBaUIsRUFBRSxNQUFNLHVCQUF1QixDQUFDO0FBTTFELElBQWEsY0FBYyxHQUEzQixNQUFhLGNBQWM7Q0FBRyxDQUFBO0FBQWpCLGNBQWM7SUFKMUIsUUFBUSxDQUFDO1FBQ04sWUFBWSxFQUFFLENBQUMsaUJBQWlCLENBQUM7UUFDakMsT0FBTyxFQUFFLENBQUMsaUJBQWlCLENBQUM7S0FDL0IsQ0FBQztHQUNXLGNBQWMsQ0FBRztTQUFqQixjQUFjIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgTmdNb2R1bGUgfSBmcm9tIFwiQGFuZ3VsYXIvY29yZVwiO1xuaW1wb3J0IHsgU3VwZXJMaWJDb21wb25lbnQgfSBmcm9tICcuL3N1cGVyLWxpYi5jb21wb25lbnQnO1xuXG5ATmdNb2R1bGUoe1xuICAgIGRlY2xhcmF0aW9uczogW1N1cGVyTGliQ29tcG9uZW50XSxcbiAgICBleHBvcnRzOiBbU3VwZXJMaWJDb21wb25lbnRdXG59KVxuZXhwb3J0IGNsYXNzIFN1cGVyTGliTW9kdWxlIHt9Il19
