64c0c03e9bd3dac5fdc2239de5552765c0410bd2
Yoonhee <yoonhee.jo.cs@gmail.com>
2020-08-01 14:34:28 -0400

    local package

---
+++ first-consumer/dist/main.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var my_first_library_1 = require("my-first-library");
+var result = my_first_library_1.CalculatorService.add(1, 2, 3);
+console.log(result);
---
+++ first-consumer/main.ts
@@ -0,0 +1,4 @@
+import { CalculatorService } from 'my-first-library';
+
+const result = CalculatorService.add(1, 2, 3);
+console.log(result);
